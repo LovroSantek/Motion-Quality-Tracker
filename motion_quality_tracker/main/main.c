31275e17ade87a2dcc2b14e623ed84bd7b6f1fae
Lovro Santek <lovro.santek29@gmail.com>
2025-04-23 21:36:21 +0200

    Add MQTT module.

--- motion_quality_tracker/main/main.c
+++ motion_quality_tracker/main/main.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
+#include "mqtt_module.h"
 
-void app_main(void)
-{
+/* Adjust based on the Wi-Fi computer is connected to.*/
+#define WIFI_SSID "Batonga Wi-Fi"
+#define WIFI_PASS "77953162"
+#define MQTT_BROKER_URI "mqtt://192.168.0.101:1883"
 
+void app_main(void) {
+    wifi_init_and_start_mqtt(WIFI_SSID, WIFI_PASS, MQTT_BROKER_URI);
+    //mqtt_publish_exercise_result(true);
 }
